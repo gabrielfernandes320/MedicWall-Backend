378b9310c7c94b17bdb95c37792978d9377fd309
Gabriel <gabrielfernandesfg@hotmail.com>
2020-02-19 23:57:55 -0300

    Role crud 1/2

--- src/controllers/RoleController.js
+++ src/controllers/RoleController.js
@@ -11,5 +11,38 @@ module.exports = {
     }
 
     return res.json(role);
+  },
+
+  async index(req, res) {
+    const { description } = req.body;
+
+    let roles = [];
+
+    if (description == null) {
+      roles = await Role.find({});
+    } else {
+      roles = await Role.find({ description: description });
+    }
+
+    console.log(description);
+    return res.json(roles);
+  },
+
+  async update(req, res) {
+    const { _id } = req.body;
+
+    const role = Role.findByIdAndUpdate(
+      { _id: "5e4c41862e362a167844a8bf" },
+      { description: "Isso" },
+      function(err, result) {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+
+    return null;
   }
 };
--- src/router.js
+++ src/router.js
@@ -4,4 +4,7 @@ const routes = express.Router();
 const roleController = require("./controllers/RoleController");
 
 routes.post("/roles", roleController.store);
+routes.get("/roles", roleController.index);
+routes.put("/roles", roleController.update);
+
 module.exports = routes;
